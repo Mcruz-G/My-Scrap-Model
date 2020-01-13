c082c93ce1f941487b44668e274b0de902d0e7d8
Miguel Cruz <mcruz@tecnoap.com>
2020-01-13 01:57:28 -0600

    First Commit

---
+++ model.py
@@ -0,0 +1,44 @@
+
+from keras import regularizers
+from keras import backend as K
+from keras.utils import multi_gpu_model
+from keras.models import Sequential, load_model, Model
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Dropout, Flatten, Activation, Add
+from keras.layers import GlobalMaxPooling2D, GaussianDropout, BatchNormalization, ReLU
+from keras.layers import GlobalAveragePooling2D, LeakyReLU, PReLU, SpatialDropout1D, SpatialDropout2D
+from keras.layers.merge import add
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint, EarlyStopping, Callback, ReduceLROnPlateau
+
+def build_model(input_shape, n_classes):
+    """
+    input_shape : tuple
+      Image Dims in a tuple
+    n_classes : int
+      Number of classes
+    """
+    
+    model = Sequential()
+    model.add(Conv2D(48, kernel_size=(3, 3), kernel_initializer='random_normal',
+                     activation='relu', input_shape=input_shape))
+    model.add(Conv2D(48, (3, 3), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Dropout(0.6))
+    model.add(MaxPooling2D(pool_size=(3, 3)))
+
+    model.add(Conv2D(128, (5, 5), activation='relu'))
+    model.add(Conv2D(128, (5, 5), activation='relu'))
+    model.add(Dropout(0.7))
+    
+    model.add(MaxPooling2D(pool_size=(3, 3))) #added 25/12/2018
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(n_classes, activation='sigmoid'))
+
+    return model
+
+    
---
+++ scrap-classifier.py
@@ -0,0 +1,61 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras import regularizers
+from keras import backend as K
+import keras
+from keras.utils import multi_gpu_model
+from keras.models import Sequential, load_model, Model
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Dropout, Flatten, Activation, Add
+from keras.layers import GlobalMaxPooling2D, GaussianDropout, BatchNormalization, ReLU
+from keras.layers import GlobalAveragePooling2D, LeakyReLU, PReLU, SpatialDropout1D, SpatialDropout2D
+from keras.layers.merge import add
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint, EarlyStopping, Callback, ReduceLROnPlateau
+from model import build_model
+
+## Constants
+width, height = 150, 150
+classes = ['cizalla','mixto','rebaba','solido','vacio']
+
+#Load Dataset
+
+trainPath = 'Dataset/train'
+validationPath = 'Dataset/validation'
+testPath = 'Dataset/test'
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        trainPath,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_directory(
+        validationPath,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='categorical')
+
+# help(train_generator)
+# # print
+# Build the model
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, width, height) 
+else:
+    input_shape = (width, height, 3)
+
+ScrapModel = build_model(input_shape = input_shape, n_classes = 5)
+ScrapModel.compile(optimizer = 'adadelta', loss = 'binary_crossentropy', metrics = ['acc'])
+ScrapModel.summary()
+ScrapModel.fit_generator(
+        train_generator,
+        steps_per_epoch=2000,
+        epochs=50,
+        validation_data=validation_generator)
+#Fit the model
